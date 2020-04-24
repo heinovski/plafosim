dcc66b3733ffca6939370998875f66d90af0c95b
Julian Heinovski <heinovski@ccs-labs.org>
2020-04-24 17:37:49 +0200

    Use a vehicle class

--- simulator.py
+++ simulator.py
@@ -27,28 +27,42 @@ number_of_cars = 100
 last_vehicle_id = -1
 cars = []
 
+
+class Vehicle:
+    'A vehicle in the simulation'
+
+    def __init__(self, vid, origin, destination, desired_speed, length, start_time):
+        '''Initialize a vehicle'''
+        self.vid = vid
+        # trip details
+        self.origin = origin
+        self.destination = destination
+        self.desired_speed = desired_speed
+        self.start_time = start_time
+        # vehicle details
+        self.posx = self.origin
+        self.lane = 0
+        self.speed = randrange(0, 28, 1)
+        self.speed = 0  # start with 0 speed for now
+        self.length = length
+
+    def stats(self):
+        '''Print stats of a vehicle'''
+        print(self.vid, "is at", self.posx, self.lane, "with", self.speed)
+
+
 # generate cars
 for num in range(0, number_of_cars):
     vid = last_vehicle_id + 1
     origin = posx = randrange(0, road_length, 1 * 1000)  # on-ramps every 1000 m
     origin = posx = 0  # start from beginning
-    lane = randrange(0, number_of_lanes, 1)
-    speed = randrange(0, 28, 1)
-    speed = 0  # start with 0 speed
     desired_speed = randrange(22, 28, 1)
     length = randrange(4, 5 + 1, 1)
     dest = randrange(posx, road_length, 1 * 1000)  # off-ramps every 1000 m
     start = randrange(0, maxstep, 1 * 60)  # in which minute to start
-    cars.append({
-        'vid': vid,
-        'posx': posx,
-        'lane': lane,
-        'speed': speed,
-        'desired_speed': desired_speed,
-        'length': length,
-        'dest': dest,
-        'start': start
-    })
+
+    cars.append(Vehicle(vid, origin, dest, desired_speed, length, start))
+
     last_vehicle_id = vid
 
 while 1:
@@ -60,20 +74,27 @@ while 1:
         exit(0)  # do we really want to exit here?
 
     for car in cars:
-        if car['start'] > step:
+        if car.start_time > step:
             # car did not start yet
             continue
         # destination reached?
-        if car['posx'] >= car['dest']:
-            print(step, ":", car['vid'], "reached its destination", car['dest'])
+        if car.posx >= car.destination:
+            # posx does not match destination
+            travelled_distance = car.posx - car.origin
+            travelled_time = step - car.start_time
+            print(step, ":", car.vid, "reached its destination", car.destination, travelled_distance, travelled_time)
             cars.remove(car)
             continue
 
         # the current status of the car
-        print(step, ":", car['vid'], "is at", car['posx'], car['posx']-car['length'], car['lane'], "with", car['speed'])
+        car.stats()
+
+        # TODO lane change
+
+        # TODO adjust
 
         # do we need to adjust our speed?
-        diff_to_desired = car['desired_speed'] - car['speed']
+        diff_to_desired = car.desired_speed - car.speed
         if diff_to_desired > 0:
             # we need to accelerate
             diff = min(diff_to_desired, max_accel)
@@ -83,27 +104,31 @@ while 1:
         else:
             # we are good
             diff = 0
-        car['speed'] += diff
+        car.speed += diff
+
+        # move
 
         # increase position according to speed
-        car['posx'] += car['speed'] * step_length
+        car.posx += car.speed * step_length
+        # TODO put destination check here?
+        # TODO use diff to destination for increasing
 
         # check for crashes of this car with any other car
         for other_car in cars:
             if car is other_car:
                 # we do not need to compare us to ourselves
                 continue
-            if other_car['start'] > step:
+            if other_car.start_time > step:
                 # other car did not start yet
                 continue
-            if car['lane'] is not other_car['lane']:
+            if car.lane is not other_car.lane:
                 # we do not car about other lanes
                 continue
-            if car['posx'] >= (other_car['posx'] - other_car['length']) and \
-                    other_car['posx'] >= (car['posx'] - car['length']):
+            if car.posx >= (other_car.posx - other_car.length) and \
+                    other_car.posx >= (car.posx - car.length):
                 # car is within the back of other_car
-                print("crash", car['vid'], car['posx'], car['length'],
-                      other_car['vid'], other_car['posx'], other_car['length'])
+                print("crash", car.vid, car.posx, car.length,
+                      other_car.vid, other_car.posx, other_car.length)
                 exit(1)
 
     step += step_length
