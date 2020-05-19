ca609371449ec5e6117cc59e4c5e1af14c5c5a06
Julian Heinovski <heinovski@ccs-labs.org>
2020-05-19 18:33:53 +0200

    Add documentation for vehicle class

--- vehicle.py
+++ vehicle.py
@@ -15,8 +15,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 class VehicleType:
+    """A collection of parameters for a concrete vehicle type"""
 
-    def __init__(self, name, length, max_speed, max_acceleration, max_deceleration):
+    def __init__(self, name: str, length: int, max_speed: int, max_acceleration: int, max_deceleration: int):
         self._name = name  # the name of a vehicle type
         self._length = length  # the length of a vehicle type
         self._max_speed = max_speed  # the maximum speed of a vehicle type
@@ -24,115 +25,114 @@ class VehicleType:
         self._max_deceleration = max_deceleration  # the maximum deceleration of a vehicle type
 
     @property
-    def length(self):
+    def length(self) -> int:
         return self._length
 
     @property
-    def max_speed(self):
+    def max_speed(self) -> int:
         return self._max_speed
 
     @property
-    def max_acceleration(self):
+    def max_acceleration(self) -> int:
         return self._max_acceleration
 
     @property
-    def max_deceleration(self):
+    def max_deceleration(self) -> int:
         return self._max_deceleration
 
 
 class Vehicle:
-    'A vehicle in the simulation'
+    'A collection of state information for a vehicle in the simulation'
 
-    def __init__(self, simulator, vid, vehicle_type, depart_position, arrival_position, desired_speed, depart_lane,
-                 depart_speed, depart_time):
+    def __init__(self, simulator, vid: int, vehicle_type, depart_position: int, arrival_position: int, desired_speed: int, depart_lane: int, depart_speed: int, depart_time: int):
         '''Initialize a vehicle'''
 
         # TODO documentation
-        self._simulator = simulator
+        self._simulator = simulator  # the simulator
 
-        self._vid = vid
-        self._vehicle_type = vehicle_type
+        self._vid = vid  # the id of the vehicle
+        self._vehicle_type = vehicle_type  # the vehicle type of the vehicle
         # trip details
-        self._depart_position = depart_position
-        self._arrival_position = arrival_position
-        self._desired_speed = desired_speed
-        self._depart_lane = depart_lane
-        self._depart_speed = depart_speed
-        self._depart_time = depart_time
+        self._depart_position = depart_position  # the departure position of the vehicle
+        self._arrival_position = arrival_position  # the arrival position of the vehicle
+        self._desired_speed = desired_speed  # the desired dirving speed of the vehicle
+        self._depart_lane = depart_lane  # the departure lane of the vehicle
+        self._depart_speed = depart_speed  # the departure speed of the vehicle
+        self._depart_time = depart_time  # the departure time of the vehicle
         # vehicle details
-        self._position = self._depart_position
-        self._lane = self._depart_lane
-        self._speed = self._depart_speed
+        self._position = self._depart_position  # the current position of the vehicle
+        self._lane = self._depart_lane  # the current lane of the vehicle
+        self._speed = self._depart_speed  # the current speed of the vehicle
         # statistics
-        self._co = 0
-        self._co2 = 0
-        self._hc = 0
-        self._pmx = 0
-        self._npx = 0
-        self._fuel = 0
+        self._co = 0  # the total co emission in g
+        self._co2 = 0  # the total co2 emission in g
+        self._hc = 0  # the total hc emission in g
+        self._pmx = 0  # the total pmx emission in g
+        self._npx = 0  # the total npx emission in g
+        self._fuel = 0  # the total fuel consumption in ml
 
     @property
-    def vid(self):
+    def vid(self) -> int:
         return self._vid
 
     @property
-    def length(self):
+    def length(self) -> int:
         return self._vehicle_type.length
 
     @property
-    def max_speed(self):
+    def max_speed(self) -> int:
         return self._vehicle_type.max_speed
 
     @property
-    def max_acceleration(self):
+    def max_acceleration(self) -> int:
         return self._vehicle_type.max_acceleration
 
     @property
-    def max_deceleration(self):
+    def max_deceleration(self) -> int:
         return self._vehicle_type.max_deceleration
 
     @property
-    def depart_position(self):
+    def depart_position(self) -> int:
         return self._depart_position
 
     @property
-    def arrival_position(self):
+    def arrival_position(self) -> int:
         return self._arrival_position
 
     @property
-    def desired_speed(self):
+    def desired_speed(self) -> int:
         return self._desired_speed
 
     @property
-    def depart_lane(self):
+    def depart_lane(self) -> int:
         return self._depart_lane
 
     @property
-    def depart_speed(self):
+    def depart_speed(self) -> int:
         return self._depart_speed
 
     @property
-    def depart_time(self):
+    def depart_time(self) -> int:
         return self._depart_time
 
     @property
-    def position(self):
+    def position(self) -> int:
         return self._position
 
     @property
-    def lane(self):
+    def lane(self) -> int:
         return self._lane
 
     @property
-    def speed(self):
+    def speed(self) -> int:
         return self._speed
 
     @property
-    def travel_distance(self):
+    def travel_distance(self) -> int:
         return self._position - self._depart_position
 
     @property
-    def travel_time(self):
+    def travel_time(self) -> int:
         return self._simulator.step - self._depart_time
 
     def info(self):
@@ -142,11 +142,14 @@ class Vehicle:
               "takes", e_remaining_travel_time)
 
     def statistics(self):
+        """Write continoius statistics"""
+
         # TODO write proper statistics
         return
 
     def finish(self):
-        """Record final statistics and do finishing stuff"""
+        """Clean up the instance of the vehicle"""
+
         if (self._position < self._arrival_position):
             return
 
@@ -170,7 +173,9 @@ class Vehicle:
             f.write(emissions)
             f.write("\n")
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return a nice string representation of a vehicle instance"""
+
         return str(self._dict_)
 
     def transmit(self, destination_vid, message):
@@ -208,7 +213,6 @@ class Vehicle:
 class PlatooningVehicle(Vehicle):
     """A vehicle that has platooning functionality enabled"""
 
-    def __init__(self, simulator, vid, vehicle_type, depart_position, arrival_position, desired_speed, depart_lane,
-                 depart_speed, depart_time):
+    def __init__(self, simulator, vid: int, vehicle_type, depart_position: int, arrival_position: int, desired_speed: int, depart_lane: int, depart_speed: int, depart_time):
         super().__init__(simulator, vid, vehicle_type, depart_position, arrival_position, desired_speed, depart_lane,
                          desired_speed, depart_time)
