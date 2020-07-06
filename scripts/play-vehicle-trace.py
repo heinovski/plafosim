99447dc6241dc9861595151de7eb7c1aedc3ce12
Julian Heinovski <heinovski@ccs-labs.org>
2020-07-06 11:29:08 +0200

    Update play trace script
    - Fix to many increases of step for simulation
    - Add functions for adding and moving
    - Add method using simple read
    - Clean up imports

--- scripts/play-vehicle-trace.py
+++ scripts/play-vehicle-trace.py
@@ -15,13 +15,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+import argparse
 import os
 import sys
 
+from random import randrange
+
 if 'SUMO_HOME' not in os.environ:
     sys.exit("please declare environment variable 'SUMO_HOME'")
-
-import argparse
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
@@ -34,6 +35,7 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
 parser = argparse.ArgumentParser(formatter_class=CustomFormatter, description="")
 parser.add_argument('trace_file', type=str, help="The name of the vehicle trace file")
 parser.add_argument('sumo_config', type=str, help="The name of the SUMO config file")
+parser.add_argument('--method', type=str, default='pandas', choices=('pandas, read'), help="The method to use for reading the trace file")
 args = parser.parse_args()
 
 tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -44,29 +46,67 @@ import traci
 sumoBinary = os.path.join(os.environ['SUMO_HOME'], 'bin/sumo-gui')
 sumoCmd = [sumoBinary, "-c", args.sumo_config]
 
-import pandas
-
-traces = pandas.read_csv(args.trace_file)
-
 traci.start(sumoCmd)
 
-step = traces.step.min()
-traci.simulationStep(step)
 
-from random import randrange
+def add_vehicle(vid: str, position: str, speed: str, lane: str):
+    traci.vehicle.add(vid, 'route', departPos=float(position), departSpeed=speed, departLane=lane, typeID='vehicle')
+    traci.vehicle.setColor(vid, (randrange(0, 255, 1), randrange(0, 255, 1), randrange(0, 255, 1)))
+    traci.vehicle.setSpeedMode(vid, 0)
+    traci.vehicle.setLaneChangeMode(vid, 0)
 
-while step <= traces.step.max():
-    for vehicle in traces.loc[traces.step == step].itertuples():
-        if str(vehicle.id) not in traci.vehicle.getIDList():
-            traci.vehicle.add(str(vehicle.id), 'route', departPos=str(vehicle.position), departSpeed=str(vehicle.speed), departLane=str(vehicle.lane), typeID='vehicle')
-            traci.vehicle.setColor(str(vehicle.id), (randrange(0, 255, 1), randrange(0, 255, 1), randrange(0, 255, 1)))
-            traci.vehicle.setSpeedMode(str(vehicle.id), 0)
-            traci.vehicle.setLaneChangeMode(str(vehicle.id), 0)
-        traci.vehicle.setSpeed(str(vehicle.id), vehicle.speed)
-        traci.vehicle.moveTo(vehID=str(vehicle.id), pos=vehicle.position, laneID='edge_0_0_%d' % vehicle.lane)
-        # traci.vehicle.moveToXY(vehID=str(vehicle.id), x=vehicle.position, y=traci.vehicle.getPosition3D(str(vehicle.id))[1], lane=vehicle.lane, edgeID='')
-    # TODO remove vehicles that arrived
-    step += 1
-    traci.simulationStep(step+1)
+
+def move_vehicle(vid: str, position: str, speed: str, lane: str):
+    traci.vehicle.setSpeed(vid, float(speed))
+    traci.vehicle.moveTo(vid, pos=float(position), laneID='edge_0_0_%s' % lane)
+    # traci.vehicle.moveToXY(vehID=str(vid), x=position, y=traci.vehicle.getPosition3D(str(vid))[1], lane=lane, edgeID='')
+
+
+def use_read():
+    step = 0
+
+    with open(args.trace_file, 'r') as file:
+        for line in file:
+            lstep, vid, position, lane, speed, duration, routeLength = line.strip(' ').split(',')
+            if lstep == "step":
+                continue
+            if int(lstep) < step:
+                print("not increasing step number")
+                exit(1)
+            elif int(lstep) > step:
+                # next step
+                traci.simulationStep(step)
+                step = int(lstep)
+                if step % 600 == 0:
+                    print("Current step:", step)
+            if vid not in traci.vehicle.getIDList():
+                add_vehicle(vid, position, speed, lane)
+            move_vehicle(vid, position, speed, lane)
+        # TODO remove vehicles that arrived
+
+
+def use_pandas():
+    import pandas
+    traces = pandas.read_csv(args.trace_file)
+
+    step = traces.step.min()
+    traci.simulationStep(step)
+
+    while step <= traces.step.max():
+        if step % 600 == 0:
+            print("Current step:", step)
+        for vehicle in traces.loc[traces.step == step].itertuples():
+            if str(vehicle.id) not in traci.vehicle.getIDList():
+                add_vehicle(str(vehicle.id), str(vehicle.position), str(vehicle.speed), str(vehicle.lane))
+            move_vehicle(str(vehicle.id), str(vehicle.position), str(vehicle.speed), str(vehicle.lane))
+        # TODO remove vehicles that arrived
+        step += 1
+        traci.simulationStep(step)
+
+
+if args.method == "read":
+    use_read()
+else:
+    use_pandas()
 
 traci.close(False)
