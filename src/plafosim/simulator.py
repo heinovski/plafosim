c382d5b5f4d14ead005ec9a71701bb02bf1dd77d
Julian Heinovski <heinovski@ccs-labs.org>
2020-07-14 19:55:59 +0200

    At collision action to sumo command

--- src/plafosim/simulator.py
+++ src/plafosim/simulator.py
@@ -367,7 +367,7 @@ class Simulator:
             import traci
 
             sumoBinary = os.path.join(os.environ['SUMO_HOME'], 'bin/sumo-gui')
-            sumoCmd = [sumoBinary, "-Q", "-c", "cfg/freeway.sumo.cfg"]
+            sumoCmd = [sumoBinary, "-Q", "-c", "cfg/freeway.sumo.cfg", '--collision.action', 'warn']
 
             traci.start(sumoCmd)
             traci.simulationStep(self._step)
