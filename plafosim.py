0426c6e5c314c6c9603df08a648cd472abff8c4f
Julian Heinovski <heinovski@ccs-labs.org>
2020-07-19 16:57:58 +0200

    Re-structure command line arguments

--- plafosim.py
+++ plafosim.py
@@ -43,57 +43,86 @@ def main():
     """)
 
     # road network properties
-    parser.add_argument('--road-length', type=int, default=100, help="The length of the road in km")
-    parser.add_argument('--lanes', type=int, default=1, help="The number of lanes")
-    parser.add_argument('--depart-interval', type=int, default=1000,
-                        help="The distance between departure positions (on-ramps) in m")
-    parser.add_argument('--arrival-interval', type=int, default=1000,
-                        help="The distance between arrival positions (off-ramps) in m")
+    road = parser.add_argument_group('road network properties')
+    road.add_argument('--road-length', type=int, default=100, help="The length of the road in km")
+    road.add_argument('--lanes', type=int, default=1, help="The number of lanes")
+    road.add_argument('--depart-interval', type=int, default=1000,
+                      help="The distance between departure positions (on-ramps) in m")
+    road.add_argument('--arrival-interval', type=int, default=1000,
+                      help="The distance between arrival positions (off-ramps) in m")
     # vehicle properties
-    parser.add_argument('--vehicles', type=int, default=100, help="The number of vehicles")
-    parser.add_argument('--max-speed', type=int, default=55,
-                        help="The maximum possible driving speed in m/s")
+    vehicle = parser.add_argument_group('vehicle properties')
+    vehicle.add_argument('--vehicles', type=int, default=100, help="The number of vehicles")
+    vehicle.add_argument('--max-speed', type=int, default=55, help="The maximum possible driving speed in m/s")
+    vehicle.add_argument('--collisions', type=lambda x: bool(strtobool(x)), default=True,
+                         choices=(True, False), help="Whether to enable collision checks")
+    vehicle.add_argument('--penetration', type=float, default=1.0,
+                         help="Penetration rate of vehicles with platooning capabilities")
     # trip properties
-    parser.add_argument('--random-depart-position', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to use a random depart position for every vehicle instead of 0 m")
-    parser.add_argument('--random-depart-lane', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to use a random depart lane for every vehicle instead of lane 0")
-    parser.add_argument('--desired-speed', type=int, default=36,
-                        help="The desired driving speed im m/s")
-    parser.add_argument('--random-desired-speed', type=lambda x: bool(strtobool(x)), default=True, choices=(True, False),
-                        help="Whether to pick a random (normally distributed) desired driving speed")
-    parser.add_argument('--speed-variation', type=float, default=0.1,
-                        help="The devation from the desired driving speed in ratio")
-    parser.add_argument('--min-desired-speed', type=int, default=22,
-                        help="The minimum desired driving speed im m/s")
-    parser.add_argument('--max-desired-speed', type=int, default=50,
-                        help="The minimum desired driving speed im m/s")
-    parser.add_argument('--random-depart-speed', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to use a random depart speed for every vehicle instead of 0 m/s")
-    parser.add_argument('--depart-desired', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether the vehicle should depart with its desired speed. Overrides random-depart-speed")
-    parser.add_argument('--depart-method', type=str, choices=('interval', 'pobability'), default='interval',
-                        help="The departure method of vehicles")
-    parser.add_argument('--depart-time-interval', type=int, default=3,
-                        help="The interval between two vehicle departures in s")
-    parser.add_argument('--random-arrival-position', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to use a random arrival position for every vehicle instead of the end of the road")
-    # mobility/application properties
-    parser.add_argument('--collisions', type=lambda x: bool(strtobool(x)), default=True, choices=(True, False),
-                        help="Whether to enable collision checks")
-    parser.add_argument('--penetration', type=float, default=1.0,
-                        help="Penetration rate of vehicles with platooning capabilities")
+    trip = parser.add_argument_group('trip properties')
+    trip.add_argument(
+        '--random-depart-position',
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        choices=(True, False),
+        help="Whether to use a random depart position for every vehicle instead of 0 m")
+    trip.add_argument(
+        '--random-depart-lane',
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        choices=(True, False),
+        help="Whether to use a random depart lane for every vehicle instead of lane 0")
+    trip.add_argument('--desired-speed', type=int, default=36, help="The desired driving speed im m/s")
+    trip.add_argument(
+        '--random-desired-speed',
+        type=lambda x: bool(strtobool(x)),
+        default=True,
+        choices=(True, False),
+        help="Whether to pick a random (normally distributed) desired driving speed")
+    trip.add_argument('--speed-variation', type=float, default=0.1,
+                      help="The devation from the desired driving speed in ratio")
+    trip.add_argument('--min-desired-speed', type=int, default=22, help="The minimum desired driving speed im m/s")
+    trip.add_argument('--max-desired-speed', type=int, default=50, help="The minimum desired driving speed im m/s")
+    trip.add_argument(
+        '--random-depart-speed',
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        choices=(True, False),
+        help="Whether to use a random depart speed for every vehicle instead of 0 m/s")
+    trip.add_argument(
+        '--depart-desired',
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        choices=(True, False),
+        help="Whether the vehicle should depart with its desired speed. Overrides random-depart-speed")
+    trip.add_argument('--depart-method', type=str, choices=('interval', 'pobability'),
+                      default='interval', help="The departure method of vehicles")
+    trip.add_argument('--depart-time-interval', type=int, default=3,
+                      help="The interval between two vehicle departures in s")
+    trip.add_argument(
+        '--random-arrival-position',
+        type=lambda x: bool(strtobool(x)),
+        default=False,
+        choices=(True, False),
+        help="Whether to use a random arrival position for every vehicle instead of the end of the road")
     # simulation properties
-    parser.add_argument('--step-length', type=int, default=1, help="The step length in s")
-    parser.add_argument('--time-limit', type=int, default=100, help="The simulation limit in h")
-    parser.add_argument('--debug', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to enable debug output")
-    parser.add_argument('--gui', type=lambda x: bool(strtobool(x)), default=False, choices=(True, False),
-                        help="Whether to enable a live sumo gui")
-    parser.add_argument('--gui-delay', type=int, default=0,
-                        help="The delay used in every simulation step to visualize the current network state in ms")
-    parser.add_argument('--result-base-filename', type=str, default='results',
-                        help="The base filename of the result files")
+    simulation = parser.add_argument_group('simulation properties')
+    simulation.add_argument('--step-length', type=int, default=1, help="The step length in s")
+    simulation.add_argument('--time-limit', type=int, default=100, help="The simulation limit in h")
+    simulation.add_argument('--debug', type=lambda x: bool(strtobool(x)), default=False,
+                            choices=(True, False), help="Whether to enable debug output")
+    simulation.add_argument('--gui', type=lambda x: bool(strtobool(x)), default=False,
+                            choices=(True, False), help="Whether to enable a live sumo gui")
+    simulation.add_argument(
+        '--gui-delay',
+        type=int,
+        default=0,
+        help="The delay used in every simulation step to visualize the current network state in ms")
+    simulation.add_argument(
+        '--result-base-filename',
+        type=str,
+        default='results',
+        help="The base filename of the result files")
 
     args = parser.parse_args()
     simulator = Simulator(
