c387640524c2098e7428a09d97b926f5fd536a22
Julian Heinovski <heinovski@ccs-labs.org>
2020-07-25 16:33:01 +0200

    Add test for ManeuverMessage

---
+++ tests/test_maneuver_message.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2020 Julian Heinovski <heinovski@ccs-labs.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+from plafosim.message import ManeuverMessage, ManeuverType, Message
+
+
+def test_creation():
+    origin = 1
+    destination = -1
+    maneuver_type = ManeuverType.JOIN  # TODO check simple integer as maneuver type
+    platoon_id = 1
+    leader_id = 1
+    string = "%d -> %d (ManeuverMessage): None" % (origin, destination)
+
+    message = ManeuverMessage(origin, destination, maneuver_type, platoon_id, leader_id)
+
+    assert(message is not None)
+    assert(message.origin is origin)
+    assert(message.destination is destination)
+    assert(message.maneuver_type is maneuver_type)
+    assert(message.data is None)
+    assert(message.platoon_id is platoon_id)
+    assert(message.leader_id is leader_id)
+    assert(str(message) == string)
+    assert(isinstance(message, Message))
