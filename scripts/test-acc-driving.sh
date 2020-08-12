571d35851bab065e16ec73654856e82532b0d38f
Julian Heinovski <heinovski@ccs-labs.org>
2020-08-12 12:53:36 +0200

    Add comparison for ACC

---
+++ scripts/test-acc-driving.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+### ACC
+
+experiment=acc
+
+echo "simulator,real,user,sys" > runtimes_$experiment.csv
+
+/usr/bin/time --format="plafosim,%e,%U,%S" --output=runtimes_$experiment.csv --append \
+    ./plafosim.py \
+    --lanes 4 \
+    --collision true \
+    --lane-changes true \
+    --vehicles 100 \
+    --time-limit 100 \
+    --road-length 100 \
+    --max-speed 55 \
+    --penetration 1 \
+    --desired-speed 36 \
+    --random-desired-speed true \
+    --speed-variation 0.1 \
+    --min-desired-speed 22 \
+    --max-desired-speed 50 \
+    --depart-method interval \
+    --depart-time-interval 3 \
+    --step 1 \
+    --result-base-filename $experiment \
+    2>&1 | tee runlog_${experiment}_plafosim
+
+# also change routes file
+/usr/bin/time --format="sumo,%e,%U,%S" --output=runtimes_$experiment.csv --append \
+    $SUMO_HOME/bin/sumo \
+    -c sumocfg/freeway-$experiment.sumo.cfg \
+    --fcd-output $experiment-traces.xml \
+    --device.fcd.deterministic \
+    --tripinfo-output $experiment-trips.xml \
+    --emission-output $experiment-emissions.xml \
+    --device.emissions.deterministic \
+    --lanechange-output $experiment-changes.xml \
+    --step-length 1 \
+    2>&1 | tee runlog_${experiment}_sumo
+
+$SUMO_HOME/tools/xml/xml2csv.py $experiment-trips.xml -o $experiment-trips.csv -s ','
+$SUMO_HOME/tools/xml/xml2csv.py $experiment-emissions.xml -o $experiment-emissions.csv -s ','
+$SUMO_HOME/tools/xml/xml2csv.py $experiment-traces.xml -o $experiment-traces.csv -s ','
+$SUMO_HOME/tools/xml/xml2csv.py $experiment-changes.xml -o $experiment-changes.csv -s ','
+
+./scripts/compare2sumo.py --experiment $experiment --desired-speed 36 --arrival-position 100000
