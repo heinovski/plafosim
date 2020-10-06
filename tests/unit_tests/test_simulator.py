3a64139b9dbb96acf79ad7238eaab6914fd08f02
Julian Heinovski <heinovski@ccs-labs.org>
2020-10-06 10:32:07 +0200

    Add test of predecessor getter

--- tests/unit_tests/test_simulator.py
+++ tests/unit_tests/test_simulator.py
@@ -46,3 +46,61 @@ class TestSimulator:
         assert(len(self.s._vehicles) == 1)
 
         # TODO test other depart methods
+
+    def test_get_predecessor(self):
+        self.setup()
+        self.s._step = 1
+        self.s._spawn_vehicle()
+#        self.s._vehicles[0]._started = True
+        self.s._step = 2
+        self.s._spawn_vehicle()
+#        self.s._vehicles[1]._started = True
+        self.s._step = 3
+        self.s._spawn_vehicle()
+#        self.s._vehicles[2]._started = True
+        self.s._vehicles[0]._position = 100
+        self.s._vehicles[0]._lane = 0
+        self.s._vehicles[1]._position = 60
+        self.s._vehicles[1]._lane = 0
+        self.s._vehicles[2]._position = 20
+        self.s._vehicles[2]._lane = 0
+
+        # test simple situation
+        assert(self.s._get_predecessor(self.s._vehicles[0]) is None)
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is self.s._vehicles[0])
+        assert(self.s._get_predecessor(self.s._vehicles[2]) is self.s._vehicles[1])
+
+        # test two vehicles same position
+        # this should throw an error in a real simulation
+        self.s._vehicles[1]._position = self.s._vehicles[0].position
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is self.s._vehicles[0])
+
+        # test two vehicles same position
+        # this should throw an error in a real simulation
+        self.s._vehicles[1]._position = self.s._vehicles[0].position - 1
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is self.s._vehicles[0])
+
+        # test two vehicles same position
+        # this should throw an error in a real simulation
+        self.s._vehicles[1]._position = self.s._vehicles[0].rear_position
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is self.s._vehicles[0])
+
+        # test small inter-vehicle gap
+        self.s._vehicles[1]._position = self.s._vehicles[0].rear_position - 1
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is self.s._vehicles[0])
+
+        # test changed vehicle order
+        self.s._vehicles[1]._position = 120
+        assert(self.s._get_predecessor(self.s._vehicles[1]) is None)
+        assert(self.s._get_predecessor(self.s._vehicles[0]) is self.s._vehicles[1])
+        assert(self.s._get_predecessor(self.s._vehicles[2]) is self.s._vehicles[0])
+
+        # test vehicle on adjacent lane
+        self.s._vehicles[0]._lane = 1
+        assert(self.s._get_predecessor(self.s._vehicles[0]) is None)
+        assert(self.s._get_predecessor(self.s._vehicles[2]) is self.s._vehicles[1])
+
+        # test vehicle next to us on adjacent lane
+        self.s._vehicles[2]._position = self.s._vehicles[0].position - 1
+        assert(self.s._get_predecessor(self.s._vehicles[0], self.s._vehicles[2].lane) is self.s._vehicles[1])
+        assert(self.s._get_predecessor(self.s._vehicles[2], self.s._vehicles[0].lane) is self.s._vehicles[0])
