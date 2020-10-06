40f7815afffbbba3b5d37fe15ed2669aecca5b19
Julian Heinovski <heinovski@ccs-labs.org>
2020-10-06 10:00:23 +0200

    Add test class for Simulator

---
+++ tests/unit_tests/test_simulator.py
@@ -0,0 +1,30 @@
+#
+# Copyright (c) 2020 Julian Heinovski <heinovski@ccs-labs.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+from plafosim.simulator import Simulator
+
+
+class TestSimulator:
+
+    def setup(self):
+        self.s = Simulator()
+        assert(self.s is not None)
+
+    def test_creation(self):
+        self.setup()
+
+        assert(len(self.s._vehicles) == 0)
+        assert(len(self.s._infrastructures) == 0)
