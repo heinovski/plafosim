0becbb682aab5b379ad613b5480c773bda845acf
Julian Heinovski <heinovski@ccs-labs.org>
2020-10-08 14:22:33 +0200

    Fix duplicated close

--- src/plafosim/simulator.py
+++ src/plafosim/simulator.py
@@ -921,7 +921,6 @@ class Simulator:
             # remove all infrastructures
             for iid in traci.polygon.getIDList():
                 traci.polygon.remove(iid)
-            traci.close(False)
             # remove all vehicles
             for vid in traci.vehicle.getIDList():
                 traci.vehicle.remove(vid, 2)
