38b08c83ee1386b10ff5da420da2060f331b629a
Julian Heinovski <heinovski@ccs-labs.org>
2020-10-09 19:14:27 +0200

    Add linebreak

--- src/plafosim/__init__.py
+++ src/plafosim/__init__.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
 from .formation_algorithm import FormationAlgorithm  # noqa 401
 from .formation_algorithm import SpeedPosition  # noqa 401
 from .infrastructure import Infrastructure  # noqa 401
--- src/plafosim/platoon.py
+++ src/plafosim/platoon.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .platooning_vehicle import PlatooningVehicle
