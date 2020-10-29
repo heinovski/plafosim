7f6755a4aaba1e4e300fc9c3cf95fd97cbd12ce0
Julian Heinovski <heinovski@ccs-labs.org>
2020-10-29 14:06:56 +0100

    Update random depart position

--- src/plafosim/simulator.py
+++ src/plafosim/simulator.py
@@ -562,6 +562,7 @@ class Simulator:
             while collision:
                 # actual calculation of position and lane
                 # always use random position for pre-filled vehicle
+                # we do not consider depart interval here since this is supposed to be a snapshot from an ealier point of simulation
                 depart_position = random.randrange(length, self.road_length, length + min_gap)
                 # always use random lane for pre-filled vehicle
                 depart_lane = random.randrange(0, self.number_of_lanes, 1)
@@ -688,7 +689,7 @@ class Simulator:
             depart_lane = 0
 
         if self._random_depart_position:
-            depart_position = random.randrange(length, self.road_length, self._depart_interval + length)
+            depart_position = random.randrange(length, self.road_length, self._depart_interval + length + min_gap)
         else:
             depart_position = length  # equal to departPos="base"
 
