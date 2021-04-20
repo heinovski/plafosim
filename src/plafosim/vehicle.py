bc9556248c07679bd99599f8a9c7fba25edfa689
Julian Heinovski <heinovski@ccs-labs.org>
2021-04-20 18:25:04 +0200

    Update documentation for vehicle

--- src/plafosim/vehicle.py
+++ src/plafosim/vehicle.py
@@ -32,11 +32,13 @@ LOG = logging.getLogger(__name__)
 
 
 class Vehicle:
-    """A collection of state information for a vehicle in the simulation
+    """
+    A collection of state information for a vehicle in the simulation.
 
     A vehicle can really be anything that can move and can be defined by a vehicle type.
     It does not necessarily be driven by a computer (i.e., autonomous).
-    However, by default it does have V2X functionality."""
+    However, by default it does have V2X functionality.
+    """
 
     def __init__(
             self,
@@ -50,7 +52,32 @@ class Vehicle:
             depart_speed: float,
             depart_time: int,
             communication_range: int):
-        """Initialize a vehicle instance"""
+        """
+        Initializes a vehicle instance.
+
+        Parameters
+        ----------
+        simulator : Simulator
+            The global simulator object
+        vid : int
+            The id of the vehicle
+        vehicle_type : VehicleType
+            The vehicle type of the vehicle
+        depart_position : int
+            The depart position of the vehicle
+        arrival_position : int
+            The arrival position of the vehicle
+        desired_speed : float
+            The desired driving speed of the vehicle
+        depart_lane : int
+            The depart lane of the vehicle
+        depart_speed : float
+            The depart speed of the vehicle
+        depart_time : int
+            The depart time of the vehicle
+        communication_range : int
+            The maximum communication range of the vehicle
+        """
 
         self._simulator = simulator  # the simulator
         self._started = False  # flag indicating whether the vehicles has actually started
@@ -88,124 +115,229 @@ class Vehicle:
 
     @property
     def vid(self) -> int:
+        """Return the id of the vehicle."""
+
         return self._vid
 
     @property
     def vehicle_type(self) -> VehicleType:
+        """Returns the VehicleType of the vehicle."""
+
         return self._vehicle_type
 
     @property
     def length(self) -> int:
+        """
+        Returns the length of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.length
 
     @property
     def max_speed(self) -> float:
+        """
+        Returns the maximum speed of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.max_speed
 
     @property
     def max_acceleration(self) -> float:
+        """
+        Returns the maximum acceleration of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.max_acceleration
 
     @property
     def max_deceleration(self) -> float:
+        """
+        Returns the maximum deceleration of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.max_deceleration
 
     @property
     def min_gap(self) -> float:
+        """
+        Returns the minimum safety gap to the vehicle in front of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.min_gap
 
     @property
     def cc_headway_time(self) -> float:
+        """
+        Returns the CC headway time of the vehicle.
+
+        This is based on the vehicle type.
+        """
+
         return self._vehicle_type.cc_headway_time
 
     @property
     def desired_headway_time(self) -> float:
+        """Returns the desired headway time of the vehicle."""
+
         return self.cc_headway_time
 
     @property
     def depart_position(self) -> int:
+        """Returns the depart position of the vehicle."""
+
         return self._depart_position
 
     @property
     def arrival_position(self) -> int:
+        """Returns the arrival position of the vehicle."""
+
         return self._arrival_position
 
     @property
     def desired_speed(self) -> float:
+        """Returns the desired driving speed of the vehicle."""
+
         return self._desired_speed
 
     @property
     def desired_gap(self) -> float:
+        """
+        Returns the desired gap to the vehicle in front of the vehicle.
+
+        This is based on the desired headway time and the current driving speed.
+        """
+
         return speed2distance(self.desired_headway_time * self.speed, self._simulator._step_length)
 
     @property
     def depart_lane(self) -> int:
+        """Returns the depart lane of the vehicle."""
+
         return self._depart_lane
 
     @property
     def depart_speed(self) -> float:
+        """Returns the depart speed of the vehicle."""
+
         return self._depart_speed
 
     @property
     def depart_time(self) -> int:
+        """Returns the depart time of the vehicle."""
+
         return self._depart_time
 
     @property
     def position(self) -> float:
+        """Returns the current position of the vehicle."""
+
         return self._position
 
     @property
     def rear_position(self) -> int:
+        """Returns the current rear position of the vehicle."""
+
         position = self.position - self.length
         assert(position >= 0)
         return position
 
     @property
     def lane(self) -> int:
+        """Returns the current lane of the vehicle."""
+
         return self._lane
 
     @property
     def speed(self) -> float:
+        """Returns the current driving speed of the vehicle."""
+
         return self._speed
 
     @property
     def acceleration(self) -> int:
+        """Returns the current acceleration of the vehicle."""
+
         return self._acceleration
 
     @property
     def cf_model(self) -> CF_Model:
+        """Returns the currently activated car following model of the vehicle."""
+
         return self._cf_model
 
     @property
     def travel_distance(self) -> float:
+        """Returns the current traveled distance of the vehicle."""
+
         return self.position - self.depart_position
 
     @property
     def travel_time(self) -> float:
+        """Returns the current traveled time of the vehicle."""
+
         return self._simulator.step - self.depart_time
 
     @property
     def blocked_front(self) -> bool:
+        """Returns whether the vehicle is currently blocked by a slow vehicle in the front."""
+
         return self._blocked_front
 
-    # krauss - single lane traffic
-    # v_safe(t) = v_lead(t) + (g(t)-g_des(t)) / (tau_b + tau)
-    # this is a simple and dumb calculation for the safe speed of a vehicle based on the positions of the predecessor and the vehicle itself
     def _safe_speed(self, speed_predecessor: float, gap_to_predecessor: float, desired_gap: float = 0, min_gap: float = 0) -> float:
+        """
+        Returns the speed which is still safe without a collision.
+
+        This is a simple and dumb calculation for the safe speed of a vehicle.
+        The calculation is is based on Krauss' single lane traffic:
+        v_safe(t) = v_lead(t) + (g(t)-g_des(t)) / (tau_b + tau)
+
+        Parameters
+        ----------
+        speed_predecessor : float
+            The driving speed of the vehicle in the front
+        gap_to_predecessor : float
+            The gap to the vehicle in the front
+        desired_gap : float, optional
+            The desired gap
+        min_gap : float, optional
+            The minimum safety gap
+        """
+
         gap_to_close = gap_to_predecessor - max(desired_gap, min_gap)  # use to close the gap
         return speed_predecessor + distance2speed(gap_to_close, self.desired_headway_time)
 
-    # krauss - single lane traffic
-    # adjust speed
-    # v_max, desired speed
-    # epsilon, dawdling of drives
-    # g_des = tau*v_lead
-    # tau, reaction time of drivers
-    # tau_b = v/b
-    # v_des(t) = min[v_max, v(t)+a(v)*step_size, v_safe(t)]
-    # v(t + step_size) = max[0, v_des(t) - epsilon]
     def new_speed(self, speed_predecessor: float, predecessor_rear_position: float) -> float:
-        """Calculate the new speed for a vehicle using the kraus model"""
+        """
+        Calculate the new speed for a vehicle using the kraus model.
+
+        This is a simple and dumb calculation for the safe speed of a vehicle.
+        The calculation is is based on Krauss' single lane traffic:
+        adjust speed
+        v_max, desired speed
+        epsilon, dawdling of drives
+        g_des = tau*v_lead
+        tau, reaction time of drivers
+        tau_b = v/b
+        v_des(t) = min[v_max, v(t)+a(v)*step_size, v_safe(t)]
+        v(t + step_size) = max[0, v_des(t) - epsilon]
+
+        Parameters
+        ----------
+        speed_predecessor : float
+            The driving speed of the vehicle in the front
+        predecessor_rear_position : float
+            The rear position of the vehicle in the front
+        """
 
         LOG.debug(f"{self.vid}'s desired speed is {self.desired_speed}m/s")
 
@@ -260,7 +392,14 @@ class Vehicle:
         return new_speed
 
     def action(self, step: int):
-        """Trigger actions of a vehicle"""
+        """
+        Triggers actions of a vehicle.
+
+        Parameters
+        ----------
+        step : int
+            The current simulation step
+        """
 
         # we started (right now)
         self._start()
@@ -276,26 +415,34 @@ class Vehicle:
             self._action(step)
 
     def _action(self, step: int):
-        """Trigger concrete actions of a Vehicle"""
+        """
+        Triggers specific actions of a vehicle.
+
+        Parameters
+        ----------
+        step : int
+            The current simulation step
+        """
 
         pass  # this vehicle has no application running
 
+    # TODO: obsolete?
     def _start(self):
-        """Start this Vehicle"""
+        """Starts this Vehicle"""
 
         if self._started:
             return
 
         self._started = True
 
-    def info(self):
-        """Return info of a Vehicle"""
+    def info(self) -> str:
+        """Returns information about the vehicle."""
 
         estimated_remaining_travel_time = round((self.arrival_position - self.position) / self.speed if self.speed > 0 else self.desired_speed, 1)
         return f"{self.vid} at {self.position}-{self.rear_position}, {self.lane} with {self.speed}, takes {estimated_remaining_travel_time}s to reach {self.arrival_position}"
 
     def _statistics(self):
-        """Write continuous statistics"""
+        """Writes continuous statistics for the vehicle."""
 
         if not self._simulator._record_prefilled and self._depart_time == -1:
             # we do not record statistics for pre-filled vehicles
@@ -372,12 +519,27 @@ class Vehicle:
                 f.write("\n")
 
     def _calculate_emission(self, a: float, v: float, f: list, scale: float) -> float:
+        """
+        Calculates the actual emission of the vehicle.
+
+        Parameters
+        ----------
+        a : float
+            The current acceleration
+        v : float
+            The current speed
+        f : list
+            The emission factors to use for current emission variable to be calculated
+        scale : float
+            The scale to normalize the calculated value
+        """
+
         if a < 0:
             return 0
         return max((f[0] + f[1] * a * v + f[2] * a * a * v + f[3] * v + f[4] * v * v + f[5] * v * v * v) / scale, 0.0)
 
     def finish(self):
-        """Clean up the instance of the vehicle"""
+        """Cleans up the instance of the vehicle."""
 
         if (self.position < self.arrival_position):
             LOG.warning(f"{self.vid}'s finish method was called even though vehicle did not arrive yet!")
@@ -449,14 +611,25 @@ class Vehicle:
                 )
 
     def __str__(self) -> str:
-        """Return a nice string representation of a vehicle"""
+        """Returns the str representation of the vehicle."""
 
         self_dict = self.__dict__.copy()
         self_dict.update({'_vehicle_type': str(self._vehicle_type)})  # use str representation of vehicle type
         return str(self_dict)
 
     def _transmit(self, destination_vid: int, message: Message) -> bool:
-        """Transmit a message of type Message"""
+        """
+        Transmits a message of type Message.
+
+        Messages are in general not used at the moment.
+
+        Parameters
+        ----------
+        destination_vid : int
+            The id of the destination vehicle
+        message : Message
+            The message to transmit
+        """
 
         if isinstance(message, Message):
             if destination_vid == -1:
@@ -471,7 +644,21 @@ class Vehicle:
         raise RuntimeError("Message is not an instance of type Message")
 
     def receive(self, message) -> bool:
-        """Receive a message of arbitrary type"""
+        """
+        Receives a message of arbitrary type.
+
+        Messgaes are in general not used at the moment.
+
+        Parameters
+        ----------
+        message :
+            The message to be received
+
+        Raises
+        ------
+        RuntimeError
+            If the message to be received is not an instance of the Message type.
+        """
 
         if self._simulator.step < self.depart_time:
             # we cannot receive anything since we did not start yet
@@ -484,13 +671,31 @@ class Vehicle:
         raise RuntimeError("Message is not an instance of type Message")
 
     def _handle_message(self, message: Message):
-        """Handle a message of arbitrary type Message"""
+        """
+        Handles a message of arbitrary type Message.
+
+        Messgaes are in general not used at the moment.
+
+        Parameters
+        ----------
+        message : Message
+            The message to be handled
+        """
 
         func = self.__class__.__dict__.get('_receive_' + message.__class__.__name__,
                                            lambda v, m: print("cannot handle message", m))
         return func(self, message)
 
     def _receive_Message(self, message: Message):
-        """Handle a message of concrete type Message"""
+        """
+        Handles a message of a specific type of Message.
+
+        Messgaes are in general not used at the moment.
+
+        Parameters
+        ----------
+        message : Message
+            The message to be received
+        """
 
         LOG.warning(f"{self.vid} received non-sense message {message}")
