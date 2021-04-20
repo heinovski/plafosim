c50d2eb300fcce97ec41e93602eeb0fa8c25d004
Julian Heinovski <heinovski@ccs-labs.org>
2021-04-20 17:51:58 +0200

    Add documentation to formation_algorithm

--- src/plafosim/formation_algorithm.py
+++ src/plafosim/formation_algorithm.py
@@ -24,7 +24,23 @@ LOG = logging.getLogger(__name__)
 
 
 class FormationAlgorithm(ABC):
+    """
+    Abstract base class for any type of platoon formation algorithm (i.e., assignment calculation).
+
+    Implementing sub-classes need to override the do_formation() method.
+    """
+
     def __init__(self, name: str, owner: object):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the formation algorithm
+        owner : object
+            The owning object that is execution this algorithm.
+            This can be either a PlatooningVehicle or an Infrastructure.
+        """
+
         self._name = name  # the name of the formation algorithm
         from .platooning_vehicle import PlatooningVehicle  # noqa 811
         from .infrastructure import Infrastructure
@@ -33,11 +49,21 @@ class FormationAlgorithm(ABC):
 
     @property
     def name(self):
+        """Prints the name of the formation algorithm."""
+
         return self._name
 
     @abstractmethod
     def do_formation(self):
+        """
+        Abstract method for performing any type of platoon formation (i.e., assignment calculation).
+
+        This methods needs to be overridden in implementing sub-classes.
+        """
+
         sys.exit("ERROR: There shouldn't be an instance of this abstract base class!")
 
     def finish(self):
+        """Reserved for future use."""
+
         pass
