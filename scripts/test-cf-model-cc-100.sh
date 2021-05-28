5788d4dd02941968ab9eccff3d092a61b98a1923
Julian Heinovski <heinovski@ccs-labs.org>
2021-05-28 12:05:26 +0200

    Add check for correct sumo version

--- scripts/test-cf-model-cc-100.sh
+++ scripts/test-cf-model-cc-100.sh
@@ -26,6 +26,9 @@ ROOT=$(pwd)/$(dirname $0)/..
 
 experiment=cc
 
+# check correct sumo version
+sumo --version | grep Version | head -n 1 | sed -E "s/.*([0-9]+\.[0-9]+\.[0-9]+)/\1/g" | xargs -i test "1.6.0" = "{}" || (echo "Incorrect SUMO version!" && exit 1)
+
 echo "simulator,real,user,sys" > ${experiment}_runtimes.csv
 
 /usr/bin/time --format="plafosim,%e,%U,%S" --output=${experiment}_runtimes.csv --append \
