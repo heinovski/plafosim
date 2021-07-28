ed577a54fc0851b96351aca316514bdd87b199f2
Julian Heinovski <heinovski@ccs-labs.org>
2021-07-28 13:26:16 +0200

    Add short option for version

--- plafosim.py
+++ plafosim.py
@@ -52,7 +52,7 @@ def main():
 
     # miscellaneous
     parser.add_argument(
-        "--version",
+        "-V, --version",
         action="version",
         version=f"%(prog)s {VERSION}"
     )
