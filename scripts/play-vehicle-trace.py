0187c7e2dbf0491b03750ffced6304c8d84e2873
Julian Heinovski <heinovski@ccs-labs.org>
2021-09-07 18:03:31 +0200

    Clean up trace player

--- scripts/play-vehicle-trace.py
+++ scripts/play-vehicle-trace.py
@@ -19,6 +19,7 @@
 import argparse
 import logging
 import os
+import pandas
 import random
 import sys
 import time
@@ -28,6 +29,11 @@ from tqdm import tqdm
 if 'SUMO_HOME' not in os.environ:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
+tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+sys.path.append(tools)
+
+import traci  # noqa 402
+
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
                       argparse.RawDescriptionHelpFormatter,
@@ -36,44 +42,74 @@ class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
     pass
 
 
-parser = argparse.ArgumentParser(formatter_class=CustomFormatter, description="")
-parser.add_argument('trace_file', type=str, help="The name of the vehicle trace file")
-parser.add_argument('--sumo-config', type=str, default="sumocfg/freeway.sumo.cfg", help="The name of the SUMO config file")
-parser.add_argument('--method', type=str, default='pandas', choices=('pandas', 'read'),
-                    help="The method to use for reading the trace file")
-parser.add_argument('--gui-delay', type=int, default=0,
-                    help="The delay used in every simulation step to visualize the current network state in ms")
-parser.add_argument('--track-vehicle', type=int, default=-1, help="The id of a vehicle to track in the gui")
-parser.add_argument('--log-level', type=str, default="warn",
-                    choices=["warn", "info", "debug"], help="Whether to enable debug output")
-parser.add_argument('--start', type=int, default=0,
-                    help="The first step to re-play from the trace file")
-parser.add_argument('--end', type=int, default=-1,
-                    help="The last step to re-play from the trace file. -1 is no limit")
-args = parser.parse_args()
+def parse_args() -> argparse.Namespace:
+    # parse some parameters
+    parser = argparse.ArgumentParser(
+        formatter_class=CustomFormatter,
+        allow_abbrev=False,
+        description="""
+    Platoon Formation Simulator (PlaFoSim) -- A simple simulator for platoon formation.
 
-# TODO add custom filter that prepends the log entry with the step time
-logging.basicConfig(level=getattr(logging, args.log_level.upper(), None), format="%(levelname)s: %(message)s")
+    Copyright (c) 2020-2021 Julian Heinovski <heinovski@ccs-labs.org>
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it under certain conditions.
+    """,
+    )
 
-tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-sys.path.append(tools)
+    parser.add_argument(
+        'trace_file',
+        type=str,
+        help="The name of the vehicle trace file"
+    )
+    parser.add_argument(
+        '--sumo-config',
+        type=str,
+        default="sumocfg/freeway.sumo.cfg",
+        help="The name of the SUMO config file"
+    )
+    parser.add_argument(
+        '--gui-delay',
+        type=int,
+        default=0,
+        help="The delay used in every simulation step to visualize the current network state in ms"
+    )
+    parser.add_argument(
+        '--track-vehicle',
+        type=int,
+        default=-1,
+        help="The id of a vehicle to track in the gui"
+    )
+    parser.add_argument(
+        '--log-level',
+        type=str,
+        default="warn",
+        choices=["warn", "info", "debug"],
+        help="Whether to enable debug output"
+    )
+    parser.add_argument(
+        '--start',
+        type=int,
+        default=0,
+        help="The first step to re-play from the trace file"
+    )
+    parser.add_argument(
+        '--end',
+        type=int,
+        default=-1,
+        help="The last step to re-play from the trace file. -1 is no limit"
+    )
 
-import traci
-
-sumoBinary = os.path.join(os.environ['SUMO_HOME'], 'bin/sumo-gui')
-sumoCmd = [sumoBinary, '-Q', '-c', args.sumo_config, '--collision.mingap-factor', '0', '--collision.action', 'none']
-
-traci.start(sumoCmd)
+    return parser.parse_args()
 
 
-def add_vehicle(vid: str, position: str, speed: str, lane: str):
+def add_vehicle(vid: str, position: str, speed: str, lane: str, track_vid: int):
     logging.debug(f"Adding vehicle {vid} at {position},{lane} with {speed}")
     traci.vehicle.add(vid, 'route', departPos=float(position), departSpeed=speed, departLane=lane, typeID='vehicle')
     traci.vehicle.setColor(vid, (random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)))
     traci.vehicle.setSpeedMode(vid, 0)
     traci.vehicle.setLaneChangeMode(vid, 0)
     # track vehicle
-    if vid == str(args.track_vehicle):
+    if vid == str(track_vid):
         traci.gui.trackVehicle("View #0", vid)
         traci.gui.setZoom("View #0", 1000000)
 
@@ -90,8 +126,19 @@ def remove_vehicle(vid: str):
     traci.vehicle.remove(vid, 2)
 
 
-def use_pandas():
-    import pandas
+def main():
+    args = parse_args()
+
+    # TODO add custom filter that prepends the log entry with the step time
+    logging.basicConfig(level=getattr(logging, args.log_level.upper(), None), format="%(levelname)s: %(message)s")
+
+    sumoBinary = os.path.join(os.environ['SUMO_HOME'], 'bin/sumo-gui')
+    sumoCmd = [sumoBinary, '-Q', '-c', args.sumo_config, '--collision.mingap-factor', '0', '--collision.action', 'none']
+
+    traci.start(sumoCmd)
+
+    logging.info("Replaying vehicle trace")
+
     traces = pandas.read_csv(args.trace_file)
     assert not traces.empty
 
@@ -103,7 +150,7 @@ def use_pandas():
         # simulate vehicles from trace file
         for vehicle in traces.loc[traces.step == step].itertuples():
             if str(vehicle.id) not in traci.vehicle.getIDList():
-                add_vehicle(str(vehicle.id), str(vehicle.position), str(vehicle.speed), str(vehicle.lane))
+                add_vehicle(str(vehicle.id), str(vehicle.position), str(vehicle.speed), str(vehicle.lane), args.track_vehicle)
             move_vehicle(str(vehicle.id), str(vehicle.position), str(vehicle.speed), str(vehicle.lane))
 
         traci.simulationStep(step)
@@ -116,52 +163,15 @@ def use_pandas():
         # sleep for visualization
         time.sleep(args.gui_delay / 1000)
 
+    # end of file
+    logging.info("Reached end of trace file")
 
-def use_read():
-    step = 0
+    # remove all vehicles
+    for vid in traci.vehicle.getIDList():
+        remove_vehicle(vid)
 
-    with open(args.trace_file, 'r') as file:
-        for line in tqdm(file, desc="Trace progress", unit="step"):
-            lstep, vid, position, lane, speed, duration, routeLength, desiredSpeed, ccTargetSpeed = line.strip(' ').split(',')
-            if lstep == "step":
-                continue
-            if int(lstep) < step:
-                sys.exit("Step number is not increasing!")
-            elif int(lstep) > step:
-                # next step
-                if int(lstep) < args.start:
-                    # we start later
-                    continue
-                if args.end != -1 and int(lstep) > args.end:
-                    # we are done
-                    return
-                traci.simulationStep(step)
-                step = int(lstep)
-
-                # sleep for visualization
-                time.sleep(args.gui_delay / 1000)
-                logging.info(f"Current step: {step}")  # TODO use tqdm
-
-            # simulate vehicles from trace file
-            if vid not in traci.vehicle.getIDList():
-                add_vehicle(vid, position, speed, lane)
-            move_vehicle(vid, position, speed, lane)
-
-            # TODO remove vehicles that arrived
+    traci.close(False)
 
 
-logging.info("Replaying vehicle trace")
-
-if args.method == "pandas":
-    use_pandas()
-else:
-    use_read()
-
-# end of file
-logging.info("Reached end of trace file")
-
-# remove all vehicles
-for vid in traci.vehicle.getIDList():
-    remove_vehicle(vid)
-
-traci.close(False)
+if __name__ == "__main__":
+    main()
