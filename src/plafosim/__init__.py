09583cc833b028aca179e96cb9892dc1f9ccfaac
Julian Heinovski <heinovski@ccs-labs.org>
2023-04-28 15:33:32 +0200

    Bump version: 0.15.4 → 0.16.0

--- src/plafosim/__init__.py
+++ src/plafosim/__init__.py
@@ -22,7 +22,7 @@ import textwrap
 
 from plafosim.util import addLoggingLevel
 
-__version__ = "0.15.4"
+__version__ = "0.16.0"
 
 __description__ = textwrap.dedent(f"""\
     Platoon Formation Simulator (PlaFoSim) -- Version {__version__}.
